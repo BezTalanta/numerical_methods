3f2dea8271b7f6ea7bb919a9eaa23b50d955f5ac
Andreyka <auvarov11@mail.ru>
2022-09-23 22:54:37 +0300

    d6

--- config/settings.py
+++ config/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.middleware.security.SecurityMiddleware",
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
